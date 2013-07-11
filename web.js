98407af885466bceb910747390408ca6c930d5ee
Ubuntu <ubuntu@ip-172-31-32-236.us-west-2.compute.internal>
2013-07-11 01:24:54 +0000

    web.js reads from disk

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var x = fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+  response.send(x.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
